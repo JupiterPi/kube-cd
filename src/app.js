eab495418f6480577664fbabc57ba0c9272fb94b
JupiterPi <jupiterpi@outlook.de>
2024-03-26 19:09:27 +0100

    bearer auth with single configurable token

--- src/app.js
+++ src/app.js
@@ -4,11 +4,22 @@ const bodyParser = require("body-parser");
 const childProcess = require("child_process");
 
 const port = process.env["PORT"] ?? 80;
+// where pushed Kubernetes configuration will be stored
 const path = process.env["KUBE_CD_PATH"] ?? "files";
+// auth token passed by clients on push, needs to be of form "token:***" or "file:***" (file must not contain a newline)
+const authTokenEnv = process.env["KUBE_CD_AUTH_TOKEN"] ?? "file:auth_token";
+const authToken = authTokenEnv.startsWith("token:")
+    ? authTokenEnv.substring("token:".length)
+    : fs.readFileSync(authTokenEnv.substring("file:".length)).toString();
 
 const app = express();
 app.use(bodyParser.text());
 app.post("/applyKubernetesConfiguration/:file", (req, res) => {
+    if ((req.headers.authorization ?? "").split(" ")[1] != authToken) {
+        res.status(401).send("Authentication required");
+        return;
+    }
+
     const fileName = req.params.file;
     const file = req.body;
 
